378bcbdad1a90da41e87983891aa4faf9e086744
Sohal <tosohal@gmail.com>
2024-08-28 17:30:52 +0200

    Add CM4F based STM32F303RE device
    This commit:
    * adds cortex m4f support for llvm
    * adds and updates the checking of stm32 devices for hal cloning

--- silicon/st/stm32devices.cmake
+++ silicon/st/stm32devices.cmake
@@ -2,11 +2,14 @@
 set (LIST_SUPPORTED_STM32_DEVICE
     "STM32F031x6"
     "STM32F072xB"
+    "STM32F303xE"
     "STM32H7A3xxQ"
     "STM32H743xx"
 )
 
 # check if there exists a cache variable within the supported list
-if (NOT ${STM32_DEVICE} IN_LIST LIST_SUPPORTED_STM32_DEVICE)
+if (${STM32_DEVICE} IN_LIST LIST_SUPPORTED_STM32_DEVICE)
+    message (STATUS "${libName}: ${STM32_DEVICE} is supported")
+else ()
     message (FATAL_ERROR "${libName}: ${STM32_DEVICE} is yet not supported")
 endif ()
--- silicon/st/stm32types.cmake
+++ silicon/st/stm32types.cmake
@@ -2,30 +2,11 @@
 # List of supported devices, add new family, device, etc. here.
 set(LIST_SUPPORTED_STM32_TYPES
     "F0"
+    "F3"
+    "F4"
     "H7"
 )
 
-# iterate in the above list to find a match in the above list
-foreach (DEVTYPE IN LISTS LIST_SUPPORTED_STM32_TYPES)
-    string (TOLOWER ${DEVTYPE} devtype)
-    #message ("${DEVTYPE}: ${devtype}")
-    foreach (DEVICE IN LISTS LIST_SUPPORTED_STM32_DEVICE)
-        #message ("${DEVICE}: ${DEVTYPE}")
-        string (FIND ${DEVICE} ${DEVTYPE} testDevType)
-        #message (${testDevType})
-        if (${testDevType} GREATER_EQUAL "0")
-            break ()
-        endif ()
-    endforeach ()
-    if (${testDevType} GREATER_EQUAL "0")
-        break ()
-    endif ()
-endforeach ()
-
-# test if there is a stm32 type that is recognized from the STM32_DEVICE variable
-if (${testDevType} GREATER_EQUAL "0")
-    set (STM32_TYPE "${devtype}" CACHE STRING "STM32 Device type in lower case" FORCE)
-    message (STATUS "${libName}: setting STM32_TYPE as ${STM32_TYPE}")
-else ()
-    message (FATAL_ERROR "${libName}: failed to find a compatible stm32 device type")
-endif ()
+# set the cmake preset environment varialble STM32_TYPE to upper case
+string (TOUPPER ${STM32_TYPE} STM32_TYPE_UPPERCASE)
+message (STATUS "${libName}: setting STM32_TYPE as ${STM32_TYPE_UPPERCASE}")

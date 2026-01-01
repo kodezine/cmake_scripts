d7f0379a40a2c69b2f6d44fbea818b43e62aafff
sohal <tosohal@gmail.com>
2026-01-01 18:21:49 +0100

    add support to h753 series

--- silicon/STMicroelectronics/stm32cubexx.devices.cmake
+++ silicon/STMicroelectronics/stm32cubexx.devices.cmake
@@ -8,7 +8,8 @@ set (
   "STM32G431xx"
   "STM32G474xx"
   "STM32H7A3xxQ"
-  "STM32H743xx")
+  "STM32H743xx"
+  "STM32H753xx")
 
 # check if there exists a cache variable within the supported list
 if (${STM32_DEVICE} IN_LIST LIST_SUPPORTED_STM32_DEVICE)
